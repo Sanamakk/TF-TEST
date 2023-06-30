c27f5c87e0cc7dbee3ba95da35b6e74740d0453e
Lucas H. Silva <109255012+Sanamakk@users.noreply.github.com>
2023-06-30 13:15:59 -0300

    Add files via upload

---
+++ infrastructure/main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "datalake" {
+  bucket = "${var.base_bucket_name}-${var.ambient}-${var.num}-${var.region}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "datalake" {
+  bucket = aws_s3_bucket.datalake.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_object" "cod_spark" {
+  bucket = aws_s3_bucket.datalake.id
+  key    = "emr-code/pyspark/job_spark_from_tf.py"
+  acl    = "private"
+  source = "../job_spark.py"
+  etag   = filemd5("../job_spark.py")
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "base_bucket_name" {
+  default = "datalake-lucas-test-tf"
+}
+
+variable "ambient" {
+  default = "production"
+}
+
+variable "num" {
+  default = "867621147051"
+}
+
+variable "region" {
+  default = "us-region-1"
+}
